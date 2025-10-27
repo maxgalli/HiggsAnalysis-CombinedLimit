0399c075aaaceb5cf6b74f927c60043069569445
Massimiliano Galli <massimiliano.galli@cern.ch>
2025-10-27 23:13:34 +0100

    have python logger mimick C++

--- python/util/logging_config.py
+++ python/util/logging_config.py
@@ -105,10 +105,9 @@ def configure_logging(verbose: Optional[int] = None) -> logging.Logger:
     logger.propagate = False
 
     log_file = os.environ.get("COMBINE_LOG_FILE")
+    formatter_pattern = "[%(levelname)s][%(module)s:%(lineno)d][%(funcName)s] %(message)s"
     if norm_verbose >= 2:
-        formatter_pattern = "%(asctime)s [%(levelname)s] [python] %(message)s"
-    else:
-        formatter_pattern = "[%(levelname)s] [python] %(message)s"
+        formatter_pattern = "%(asctime)s " + formatter_pattern
 
     console_handler = logging.StreamHandler(stream=sys.__stdout__)
     console_handler.setFormatter(_ColourFormatter(formatter_pattern))
